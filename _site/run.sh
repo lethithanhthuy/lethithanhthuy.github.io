5ab233fc16ab6348f3ad6067b6c719d7df0002f7
truongdq <truongdq54@gmail.com>
2015-11-20 22:30:25 +0900

    Fix font

---
+++ _site/run.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+#
+# run.sh
+# Copyright (C) 2015 truong-d <truong-d@dellxps>
+#
+# Distributed under terms of the MIT license.
+#
+
+# curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -
+# sudo apt-get install nodejs zip jekyll libjs-lodash  node-lodash  graphicsmagick
+# sudo npm install -g grunt-cli
+# if above commands do not contain npm -> apt-get install npm
+
+# to build on local: change destination: _site in _config.yml because grunt has problem with
+# underscore
+
+# run local
+# 1. grunt
+# 2. jekyll serve
+
+# deploy
+grunt deploy
